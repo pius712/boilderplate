00c5be7c078c439c4aaa41a7f661437f61417d0e
pius712 <aksfbsgnlfls@gmail.com>
2023-09-03 20:46:29 +0900

    fix: add config

---
+++ src/main/java/com/example/boilerplate/core/controller/advice/CoreApiAdvice.java
@@ -0,0 +1,7 @@
+package com.example.boilerplate.core.controller.advice;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(basePackageClasses = {})
+public class CoreApiAdvice {
+}
